f03bf1640031ef1b9cabdc06f2acd5e7edd06b15
koojunho <koojh@9cells.com>
2016-02-16 02:39:39 +0900

    github 로그인 버튼 다중 클릭 방지, 에러 안내

--- resources/views/auth/pages/login.blade.php
+++ resources/views/auth/pages/login.blade.php
@@ -1,8 +1,17 @@
 @extends('app')
 
 @section('content')
+<div class="alert alert-info" role="alert">신규 스킨 적용 예정</div>
+
 <div class="row">
     <div class="col-lg-12">
+        <p>
+            알 수 없는 이메일 주소로 로그인이 안되시는 분들은 <br/>
+            Github 설정 > Personal settings > Profile > Public profile > Public email > Don’t show my email address<br/>
+            위처럼 되어있기 때문인데 노출 가능한 E-mail 을 선택해주시면 로그인이 됩니다.<br/>
+            다른 로그인 방법을 추가하도록 노력하겠습니다.<br/>
+            <span style="color: red;">탈퇴 기능이 아직 제공되지 않으므로 신중히 가입해주십쇼.</span>
+        </p>
         @if($login_status === 'login_status_success')
         <div class="alert alert-success" role="alert">로그인에 성공했습니다.</div>
         @elseif($login_status === 'login_status_fail_email')
@@ -26,14 +35,17 @@
 
 @section('script')
 <script>
-    $(function() {
-        $('#btn-login').click(function(e) {
+    $(function () {
+        $('#btn-login').click(function (e) {
+            if ($(this).attr('disabled')) {
+                return false;
+            }
             $(this).text('Github 로그인 중...');
             $(this).attr('disabled', true);
             return true;
         });
 
-        $('#btn-logout').click(function(e) {
+        $('#btn-logout').click(function (e) {
             $(this).attr('disabled', true);
             $(this).parents('form').submit();
         });
