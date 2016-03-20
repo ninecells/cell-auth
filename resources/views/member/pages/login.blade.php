71665e77c24d870ae31704d12ff9f40502b4aea4
koojunho <koojh@9cells.com>
2016-03-21 05:28:36 +0900

    소셜 로그인 뷰 개선

--- resources/views/member/pages/login.blade.php
+++ resources/views/member/pages/login.blade.php
@@ -15,6 +15,10 @@
                 </div>
                 @endif
 
+                <p><span style="color: red;">탈퇴 기능이 아직 제공되지 않으므로 신중히 가입해주십쇼.</span></p>
+                <a id="btn-login" class="btn btn-default" href="/auth/github">Github 로그인</a>
+                <hr/>
+
                 <form method="POST" action="/auth/login" accept-charset="UTF-8" class="form-horizontal">
                     {{ csrf_field() }}
 
@@ -48,27 +52,8 @@
                             &nbsp;&nbsp;&nbsp;<a href="/auth/register">Register</a>
                         </div>
                     </div>
+
                 </form>
-
-            </div>
-        </div>
-    </div>
-</div>
-
-<div class="row">
-    <div class="col-md-12">
-        <div class="panel panel-default">
-            <div class="panel-heading">Github 로그인</div>
-            <div class="panel-body">
-
-                <p>
-                    알 수 없는 이메일 주소로 로그인이 안되시는 분들은 <br/>
-                    Github 설정 > Personal settings > Profile > Public profile > Public email > Don’t show my email address<br/>
-                    위처럼 되어있기 때문인데 노출 가능한 E-mail 을 선택해주시면 로그인이 됩니다.<br/>
-                    다른 로그인 방법을 추가하도록 노력하겠습니다.<br/>
-                    <span style="color: red;">탈퇴 기능이 아직 제공되지 않으므로 신중히 가입해주십쇼.</span>
-                </p>
-                <a id="btn-login" class="btn btn-default" href="/auth/github">Github 로그인</a>
 
             </div>
         </div>
