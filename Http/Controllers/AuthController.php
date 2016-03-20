30ee534c7d3d94e1781e7a2879f57bea70a3edba
koojunho <koojh@9cells.com>
2016-03-21 05:22:04 +0900

    SocialLogin 여러 프로바이더 지원 및 문제 해결

--- Http/Controllers/AuthController.php
+++ Http/Controllers/AuthController.php
@@ -16,17 +16,6 @@ use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 
 class AuthController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Registration & Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users, as well as the
-    | authentication of existing users. By default, this controller uses
-    | a simple trait to add these behaviors. Why don't you explore it?
-    |
-    */
-
     use AuthenticatesAndRegistersUsers, ThrottlesLogins;
 
     protected $redirectTo = '/';
@@ -65,89 +54,52 @@ class AuthController extends Controller
         return Response::json(['redirect' => '/auth/login']);
     }
 
-    /**
-     * Redirect the user to the GitHub authentication page.
-     *
-     * @return Response
-     */
-    public function redirectToProvider()
+    public function redirectToProvider($provider)
     {
-        return Socialite::driver('github')
-            ->scopes(['user:email'])
+        return Socialite::driver($provider)
             ->redirect();
     }
 
-    /**
-     * Obtain the user information from GitHub.
-     *
-     * @return Response
-     */
-    public function handleProviderCallback(Request $request)
+    public function handleProviderCallback(Request $request, $provider)
     {
-        $guest = null;
-        try {
-            $guest = Socialite::driver('github')->user();
-
-            if (!$guest || !isset($guest->user)) {
-                throw new Exception('login_status_fail');
-            } else if (!isset($guest->user['email'])) {
-                throw new Exception('login_status_fail_email');
-            } else if (!isset($guest->user['login'])) {
-                throw new Exception('login_status_fail_login');
-            }
-        } catch (Exception $e) {
-            $request->session()->flash('login_status', $e->getMessage());
-            Log::error($e->getMessage() . ' ' . print_r(get_object_vars($guest), true));
-            return redirect('auth/login');
-        }
-
-        $authUser = $this->findOrCreateUser($request, $guest);
-
+        $guest = Socialite::driver($provider)->user();
+        $authUser = $this->findOrCreateUser($provider, $guest);
         Auth::login($authUser, true);
 
         return redirect("members/{$authUser->id}");
     }
 
-    /**
-     * Return user if exists; create and return if doesn't
-     *
-     * @param $githubUser
-     * @return User
-     */
-    private function findOrCreateUser(Request $request, $guest)
+    private function findOrCreateUser($provider, $guest)
     {
-        $ST_GITHUB = 'github';
         $user = null;
 
         $social = SocialLogin::where([
             'social_id' => $guest->id,
-            'social_type' => $ST_GITHUB,
+            'social_type' => $provider,
         ])->first();
 
         if ($social) {
-            $user = $social->user;
-            if ($user) {
-                goto success;
-            }
+            // 이미 알고있는 사용자
+            return $social->user;
         }
 
-        $user = User::where('email', $guest->email)->first();
+        $name = $guest->name;
+        $email = $guest->email ?: "{$guest->id}@{$provider}.com";
+
+        $user = User::where('email', $email)->first();
         if (!$user) {
             $user = User::create([
-                'name' => $guest->user['login'],
-                'email' => $guest->user['email'],
+                'name' => $name,
+                'email' => $email,
             ]);
         }
 
         SocialLogin::create([
             'user_id' => $user->id,
             'social_id' => $guest->id,
-            'social_type' => $ST_GITHUB,
+            'social_type' => $provider,
             'avatar' => $guest->avatar,
         ]);
-
-        success:
-        $request->session()->flash('login_status', 'login_status_success');
 
         return $user;
     }
--- Http/routes.php
+++ Http/routes.php
@@ -7,12 +7,12 @@ Route::group(['middleware' => ['web']], function () {
         Route::get('auth/register', 'AuthController@getRegister');
         Route::post('auth/register', 'AuthController@postRegister');
 
-        Route::get('auth/github', 'AuthController@redirectToProvider');
-        Route::get('auth/github/callback', 'AuthController@handleProviderCallback');
-
         Route::get('auth/login', 'AuthController@getLogin');
         Route::post('auth/login', 'AuthController@postLogin');
         Route::post('auth/logout', 'AuthController@logout');
+
+        Route::get('auth/{provider}', 'AuthController@redirectToProvider');
+        Route::get('auth/{provider}/callback', 'AuthController@handleProviderCallback');
 
         Route::get('members/{member_id}', 'MemberController@GET_member')->name('ncells::url.auth.member_profile');
     });
